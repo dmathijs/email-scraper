292081f1906c25e101d992e4ea9e212f4ba14c4c
Diederik Mathijs <diederik.mathijs@gmail.com>
2020-08-10 09:11:25 +0200

    Perf test improvements

---
+++ perf_test/test.js
@@ -0,0 +1,26 @@
+const {scrapeEmails} = require('../scraper.js')
+const fs = require('fs')
+
+
+fs.readFile('test.html', 'utf8', (err, data) => {
+    const date1 = Date.now();
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    scrapeEmails(data)
+    const date2 = Date.now();
+
+    console.log(date2 - date1, "ms")
+})
